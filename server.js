dbcdbd943fe9a4d3497024cff96704af25c0c344
Darkday32 <57805082+Darkday32@users.noreply.github.com>
2020-07-12 11:41:29 +0300

    Fix build

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/cases', cases);
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-
+  
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
